9bf3790dea65a190f5b3f02d20e1bd554832449e
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-02-14 22:09:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,6 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 
+hiddenElements.forEach((el) => {
+    observer.observe(el);
+})
